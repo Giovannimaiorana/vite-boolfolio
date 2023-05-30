c7ada4ce7f93111a41bf08a7c9058d98eea87e69
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-05-30 21:53:37 +0200

    esercizio completo + bonus

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,16 +7,28 @@ export default {
         return {
             projects: [],
             baseUrl: 'http://localhost:8000',
+            currentPage: 1,
+            lastPage: null,
         }
     },
     methods: {
-        getProjects() {
+        getProjects(selectPage) {
 
-            axios.get(`${this.baseUrl}/api/projects`)
+            axios.get(`${this.baseUrl}/api/projects`,
+                {
+                    params: {
+                        page: selectPage,
+                    }
+                }
+
+            )
                 .then(response => {
                     console.log(response);
-                    this.projects = response.data.results;
-                });
+                    this.projects = response.data.results.data;
+                    this.currentPage = response.data.results.current_page;
+                    this.lastPage = response.data.results.last_page;
+                })
+
         },
         troncateText(text) {
             if (text && text.length > 10) {
@@ -25,7 +37,7 @@ export default {
         }
     },
     mounted() {
-        this.getProjects();
+        this.getProjects(1);
     }
 }
 </script>
@@ -33,7 +45,7 @@ export default {
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-3" v-for="project in projects">
+            <div class="col-4" v-for="project in projects">
                 <div class="card mb-2" style="width: 18rem;">
                     <img v-if="project.preview_image" :src="`${this.baseUrl}/storage/${project.preview_image}`"
                         class="card-img-top" />
@@ -53,6 +65,19 @@ export default {
                         <a href="#" class="btn btn-primary">Visualizza altro</a>
                     </div>
                 </div>
+
+
+
+            </div>
+            <div class="container d-flex justify-content-center mt-5">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <li class="page-item"><button class="page-link" @click="getProjects(currentPage - 1)"
+                                :class="{ 'disabled': currentPage == 1 }">Previous</button></li>
+                        <li class="page-item"><button class="page-link" @click="getProjects(currentPage + 1)"
+                                :class="{ 'disabled': currentPage == lastPage }">Next</button></li>
+                    </ul>
+                </nav>
 
             </div>
 

3c1f3b862c69214690dec9719fba812908b02dcf
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-05-30 18:15:17 +0200

    fine esercizio

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -27,6 +27,23 @@ export default {
 
 <template>
     <div class="container">
+        <div class="row">
+            <div class="col-3" v-for="project in projects">
+                <div class="card mb-2" style="width: 18rem;">
+                    <img v-if="project.preview_image" :src="`${this.baseUrl}/storage/${project.preview_image}`"
+                        class="card-img-top" />
+                    <img v-else src="https://cdn.icon-icons.com/icons2/1462/PNG/512/120nophoto_100007.png"
+                        class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ project.title }}</h5>
+                        <p class="card-text">{{ project.description }}</p>
+                        <a href="#" class="btn btn-primary">Visualizza altro</a>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
 
     </div>
 </template>

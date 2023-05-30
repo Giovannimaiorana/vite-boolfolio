1d17e3ad7fde91db9e128ed6131b66a169bcbdc5
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-05-30 17:31:31 +0200

    first commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,34 @@
+<script>
+import axios from 'axios';
+export default {
+    name: "AppMain",
+
+    data() {
+        return {
+            projects: [],
+            baseUrl: 'http://localhost:8000',
+        }
+    },
+    methods: {
+        getProjects() {
+
+            axios.get(`${this.baseUrl}/api/projects`)
+                .then(response => {
+                    console.log(response);
+                    this.projects = response.data.results;
+                });
+        }
+    },
+    mounted() {
+        this.getProjects();
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+</template>

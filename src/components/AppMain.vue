f49e2c2f1361ac51f0fbb72e3a47fcafc7c798b8
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-05-31 15:04:39 +0200

    inserimento props

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,20 +1,25 @@
 <script>
 import axios from 'axios';
+import AppProject from './AppProject.vue';
+import { store } from '../store.js';
 export default {
     name: "AppMain",
 
     data() {
         return {
             projects: [],
-            baseUrl: 'http://localhost:8000',
+            store,
             currentPage: 1,
             lastPage: null,
         }
     },
+    components: {
+        AppProject,
+    },
     methods: {
         getProjects(selectPage) {
 
-            axios.get(`${this.baseUrl}/api/projects`,
+            axios.get(`${this.store.baseUrl}/api/projects`,
                 {
                     params: {
                         page: selectPage,
@@ -30,11 +35,7 @@ export default {
                 })
 
         },
-        troncateText(text) {
-            if (text && text.length > 10) {
-                return text.substr(0, 50) + `...`
-            }
-        }
+
     },
     mounted() {
         this.getProjects(1);
@@ -46,28 +47,7 @@ export default {
     <div class="container">
         <div class="row">
             <div class="col-4" v-for="project in projects">
-                <div class="card mb-2" style="width: 18rem;">
-                    <img v-if="project.preview_image" :src="`${this.baseUrl}/storage/${project.preview_image}`"
-                        class="card-img-top" />
-                    <img v-else src="http://www.tecno-store.it/wp-content/uploads/immagine-non-disponibile.png"
-                        class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ project.title }}</h5>
-                        <p class="card-text">tecnologia: <br />
-                            <span v-for="technology in project.technologies">
-                                {{ technology?.name }}<br>
-                            </span>
-                        </p>
-                        <p class="card-text">tipo: <br />
-                            <span>{{ project.type?.name }}</span>
-                        </p>
-                        <p class="card-text">{{ troncateText(project.description) }}</p>
-                        <a href="#" class="btn btn-primary">Visualizza altro</a>
-                    </div>
-                </div>
-
-
-
+                <AppProject :project="project"></AppProject>
             </div>
             <div class="container d-flex justify-content-center mt-5">
                 <nav aria-label="Page navigation example">
---
+++ src/components/AppProject.vue
@@ -0,0 +1,41 @@
+<script>
+import { store } from '../store.js';
+export default {
+    name: 'AppProject',
+    props: ['project'],
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        troncateText(text) {
+            if (text && text.length > 10) {
+                return text.substr(0, 50) + `...`
+            }
+        }
+    }
+};
+
+</script>
+
+<template>
+    <div class="card mb-2" style="width: 18rem;">
+        <img v-if="project.preview_image" :src="`${this.store.baseUrl}/storage/${project.preview_image}`"
+            class="card-img-top" />
+        <img v-else src="http://www.tecno-store.it/wp-content/uploads/immagine-non-disponibile.png" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text">tecnologia: <br />
+                <span v-for="technology in project.technologies">
+                    {{ technology?.name }}<br>
+                </span>
+            </p>
+            <p class="card-text">tipo: <br />
+                <span>{{ project.type?.name }}</span>
+            </p>
+            <p class="card-text">{{ troncateText(project.description) }}</p>
+            <a href="#" class="btn btn-primary">Visualizza altro</a>
+        </div>
+    </div>
+</template>

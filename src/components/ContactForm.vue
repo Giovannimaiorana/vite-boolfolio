82e355408e46a6a3339278d5c46e2fa1307f3e90
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-06-06 18:25:30 +0200

    esercizio completo con validazioni

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -11,12 +11,14 @@ export default {
             message: '',
             success: false,
             sending: false,
+            errors: {},
         }
     },
     methods: {
         sendForm() {
             this.success = false;
             this.sending = true;
+            this.errors = {};
 
             axios.post(`${this.store.baseUrl}/api/contacts`,
                 {
@@ -31,8 +33,8 @@ export default {
                         this.message = '';
                         this.success = true;
                         this.sending = false;
-
-
+                    } else {
+                        this.errors = response.data.errors;
                     }
                 }).catch(error => {
                     this.sending = false;
@@ -49,15 +51,26 @@ export default {
     <form @submit.prevent="sendForm()">
         <div class="mb-3">
             <label for="email" class="form-label">La tua Email</label>
-            <input type="email" class="form-control" id="email" v-model="email">
+            <input type="email" class="form-control" id="email" v-model="email" :class="{ 'is-invalid': errors.email }">
+            <div class="invalid-feedback" v-for="error in errors.email">
+                {{ error }}
+            </div>
+
         </div>
         <div class="mb-3">
             <label for="name" class="form-label">Il tuo Nome </label>
-            <input type="text" class="form-control" id="name" v-model="name">
+            <input type="text" class="form-control" id="name" v-model="name" :class="{ 'is-invalid': errors.name }">
+            <div class="invalid-feedback" v-for="error in errors.name">
+                {{ error }}
+            </div>
         </div>
         <div class="mb-3 ">
             <label for="message">Il tuo messaggio</label>
-            <textarea class="form-control" placeholder="Scrivi il tuo Messaggio" id="message" v-model="message"></textarea>
+            <textarea class="form-control" placeholder="Scrivi il tuo Messaggio" id="message" v-model="message"
+                :class="{ 'is-invalid': errors.message }"></textarea>
+            <div class="invalid-feedback" v-for="error in errors.message">
+                {{ error }}
+            </div>
         </div>
         <button type="submit" class="btn btn-primary" :disabled="sending">
             {{ sending ? 'Invio in corso...' : 'Invia Messaggio' }}
